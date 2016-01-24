6f33483eb2e46157e3f9253536ac734eb970f55e
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2016-01-24 09:26:15 -0800

    Move download_check_extract in spark_helper module

--- notebooks/spark_helper.py
+++ notebooks/spark_helper.py
@@ -1,5 +1,45 @@
 import pyspark
 import re
+import hashlib
+import os
+import tarfile
+
+from urllib.request import urlretrieve
+from urllib.parse import urljoin, urlparse
+
+EXT = ('.tgz', '.tar.gz')
+
+CWD =  os.getcwd()
+
+def md5(fname):
+    hash = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
+
+def download_check_extract(path, url, files, md5s):
+    os.chdir(CWD)    
+    # Create destination folder if it does not exist
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    # Download archives and verify checkums
+    for file_, md5_ in zip(files, md5s):
+        file_path = os.path.join(path, file_)
+        if not os.path.exists(file_path):
+            urlretrieve(urljoin(url,file_), file_path)
+        else:
+            print('File already downloaded, checking MD5.')
+        correct = md5_ == md5(file_path)
+        print("{file} {status}".format(file=file_, status=('OK' if correct else 'BAD')))
+        
+        # Extract tarball if checksum is correct
+        if correct:
+            if any(file_.endswith(ext) for ext in EXT):
+                with tarfile.open(file_path, 'r:gz') as tar:
+                    os.chdir(path)
+                    tar.extractall()
 
 def url_path_to_dict(path):
     pattern = (r'^'
